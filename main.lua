37b32da721515446b8e97e66c865cc7c41862eb2
CKScripts <78039720+CKScripts@users.noreply.github.com>
2022-08-01 00:43:38 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,8 @@
-print("This is a test!")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Remote = ReplicatedStorage:FindFirstChild("\na\na\na")
+
+if Remote then
+	print("Sucessfully Loaded.")
+else
+	print("Error: No Remote Found.")
+end
